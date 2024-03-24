282c7f5c52b9bde381f51711a96d29362e1f9b8d
RafaelMVilla <rafael.march.v@gmail.com>
2024-03-24 17:07:17 -0300

    Exercicio02

---
+++ classes/Automovel.java
@@ -0,0 +1,13 @@
+package classes;
+
+public class Automovel {
+    
+    public double distancia;
+    public double combGasto;
+
+
+    public double resultado(){
+        return (this.distancia / this.combGasto);
+    }
+}
+
